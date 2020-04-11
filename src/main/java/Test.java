7135a022564a4aa0b888b44c577d3cbbba203a43
ybuq <ybuq@atguigu.com>
2020-04-11 17:43:24 +0800

    master add Test class

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -2,5 +2,6 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("张永平master");
    System.out.println("张大仙master");
+        System.out.println("woaini");
     }
 }
