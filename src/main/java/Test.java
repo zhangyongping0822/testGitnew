6c878457e08673d0036334ab9e7cc44db07275de
zhangyongping0822 <63491082+zhangyongping0822@users.noreply.github.com>
2020-05-27 11:40:57 +0800

    xxx

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,7 +1,7 @@
 public class Test {
     public static void main(String[] args) {
         System.out.println("张永平master");
-   System.out.println("张大仙master");
+   System.out.println("张23master");
         System.out.println("woaini");
     }
 }
