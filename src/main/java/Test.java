7c67c0f6e877d24ceb9caf138f104aec250449bc
zhangyongping0822 <63491082+zhangyongping0822@users.noreply.github.com>
2020-06-08 20:16:18 +0800

    新建了dev分支

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,7 +1,7 @@
 public class Test {
     public static void main(String[] args) {
         System.out.println("张永平master");
-        System.out.println("gjlmaster");
+        System.out.println("gjlmaster1");
         System.out.println("woaini");
     }
 }
