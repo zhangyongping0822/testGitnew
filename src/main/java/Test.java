b4760613e7e6f0387c48ce09f9439ac99b682499
zhangyongping0822 <63491082+zhangyongping0822@users.noreply.github.com>
2020-06-08 20:09:35 +0800

    第一次修改

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,7 +1,7 @@
 public class Test {
     public static void main(String[] args) {
         System.out.println("张永平master");
-   System.out.println("张23master");
+        System.out.println("gjlmaster");
         System.out.println("woaini");
     }
 }
